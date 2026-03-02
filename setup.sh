4c6ecd78b0c753b12b774fea664ddaf407df39c4
n4n5 <its.just.n4n5@gmail.com>
2026-03-01 19:48:06 -0700

    update file

--- setup.sh
+++ setup.sh
@@ -8,11 +8,27 @@ SCRIPT_PATH="$(
 
 scripts=$(fdfind -H -a -E .git -t f --full-path "./\..*")
 
+grey=$(tput setaf 244)
+blue=$(tput setaf 4)
+green=$(tput setaf 2)
+red=$(tput setaf 1)
+reset=$(tput sgr0)
+
 for one_script in $scripts; do
     local_path=${one_script//$SCRIPT_PATH/}
     dest_path="$HOME/$local_path"
-    echo -n "Installing '$local_path' at '$dest_path'..."
+    dir_path=$(dirname "$dest_path")
+    if [ ! -d $dir_path ]; then
+        echo "Creating '${blue}$dir_path${reset}'"
+	mkdir -p "$dir_path"
+    fi
+    echo -n "Installing '${grey}$local_path${reset}' at '${blue}$dest_path${reset}'..."
     ln -s -f "$one_script" "$dest_path"
+    result=$?
     sleep 0.1
-    echo "done"
+    if [ "$result" -eq 0 ]; then
+        echo "${green}done${reset}"
+    else
+        echo "${red}failed${reset}"
+    fi
 done
